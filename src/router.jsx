bf63b9f985bf51b6f1a5f4f1b4bce8d264d44f26
Lucas Vanderlei <lucasavprc@gmail.com>
2021-04-09 20:49:42 -0300

    Created react app with CRA. Added eslint and prettier. Added router.

---
+++ src/router.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const routes = [];
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      {routes.map(({ path, component }) => (
+        <Route exact key={path} path={path} component={component} />
+      ))}
+    </Switch>
+  </BrowserRouter>
+);
+export default Routes;
