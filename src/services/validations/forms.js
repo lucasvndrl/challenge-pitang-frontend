8646cc44f235198faa0678479283752bccc06043
Lucas Vanderlei <lucasavprc@gmail.com>
2021-04-22 22:45:54 -0300

    Fixed appointment form validation.

--- src/services/validations/forms.js
+++ src/services/validations/forms.js
@@ -2,7 +2,10 @@ import * as Yup from 'yup';
 import { validateMinAndMaxTime } from './functions';
 
 export const schedulingFormSchema = Yup.object({
-  name: Yup.string().required('Required!'),
+  name: Yup.string()
+    .min(3)
+    .required('Required!')
+    .matches(/^[A-Za-zà-úÀ-Ú ]+$/, 'Nome deve conter apenas letras'),
   birthday: Yup.date().required('Required!').nullable(),
   selectedDate: Yup.date()
     .test('selectedDate', 'Horário inválido!', () => validateMinAndMaxTime)
